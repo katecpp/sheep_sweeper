3acf7ebdd2b25b22e781085eb832d0e6f7419c3f
katecpp <kasia.macias@gmail.com>
2015-12-10 20:37:57 +0100

    Removed debug from Settings window

--- CSettingsDialog.cpp
+++ CSettingsDialog.cpp
@@ -1,7 +1,6 @@
 #include <CSettingsDialog.h>
 #include <ui_CSettingsDialog.h>
 #include <Constants.h>
-#include <QDebug>
 
 namespace SSw
 {
@@ -16,8 +15,8 @@ CSettingsDialog::CSettingsDialog(QWidget *parent) :
     connect(ui->heightSpinBox, SIGNAL(valueChanged(int)), this, SLOT(updateRange()));
     connect(ui->widthSpinBox, SIGNAL(valueChanged(int)), this, SLOT(updateRange()));
 
-    ui->heightSpinBox->setMinimum(2);
-    ui->widthSpinBox->setMinimum(2);
+    ui->heightSpinBox->setMinimum(3);
+    ui->widthSpinBox->setMinimum(3);
 }
 
 CSettingsDialog::~CSettingsDialog()
@@ -37,15 +36,10 @@ bool CSettingsDialog::getPreferences(SPreferences &prefs, QWidget *parent)
 
     if (dialog.exec() == QDialog::Accepted)
     {
-        qDebug() << "Accepted";
-        prefs.width  = dialog.ui->heightSpinBox->value();
-        prefs.height   = dialog.ui->widthSpinBox->value();
+        prefs.width   = dialog.ui->heightSpinBox->value();
+        prefs.height  = dialog.ui->widthSpinBox->value();
         prefs.sheep   = dialog.ui->sheepSpinBox->value();
         retValue = true;
-    }
-    else
-    {
-        qDebug() << "Rejected";
     }
 
     return retValue;
@@ -53,7 +47,6 @@ bool CSettingsDialog::getPreferences(SPreferences &prefs, QWidget *parent)
 
 void CSettingsDialog::updateRange()
 {
-    qDebug() << "Update ranges";
     int32_t size = ui->heightSpinBox->value() * ui->widthSpinBox->value();
     ui->sheepSpinBox->setRange(MIN_DENSITY * size, MAX_DENSITY * size);
 }
